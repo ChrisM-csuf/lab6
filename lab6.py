89107bbbc5569d03f26e70bc46f5456fb5d87d2a
chrism <chrism@kingorange.com>
2021-05-08 09:17:35 -0700

    structured - english

---
+++ lab6.py
@@ -0,0 +1,10 @@
+"""
+to start, we generate a random number between 1 and 20
+based on that number we check if it falls into a certain range
+if number > 15 we say charries
+>5 plum
+> 2 melon
+> 1 bell
+outside of those its bar
+
+we iterate ove it using a loop 3 times
